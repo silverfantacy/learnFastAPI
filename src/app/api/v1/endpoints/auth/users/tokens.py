5f8330d8069ad4d8fd955a73701b8769c4bf8790
Zero <silverfantacy@gmail.com>
2023-09-27 23:52:55 +0800

    feat: get username from token info

--- src/app/api/v1/endpoints/auth/users/tokens.py
+++ src/app/api/v1/endpoints/auth/users/tokens.py
@@ -6,3 +6,7 @@ router = fastapi.APIRouter()
 @router.post("")
 def create_jwt_token():
     return {"access_token": "access_token", "refresh_token": "refresh_token"}
+
+@router.get("/info")
+def get_jwt_token_info():
+    return {"username": "test"}
--- src/tests/test_services/test_token.py
+++ src/tests/test_services/test_token.py
@@ -8,7 +8,15 @@ from app import main
 async def test_create_jwt_token_by_username_and_passowrd():
     async with httpx.AsyncClient(app=main.app, base_url="http://test") as client:
         resp = await client.post(
-            "/v1/auth/users/tokens",
-            json={"username": "test", "password": "testme"}
+            "/v1/auth/users/tokens", json={"username": "test", "password": "testme"}
         )
         assert resp.status_code == 200
+        data = resp.json()
+        assert (access_token := data["access_token"])
+        assert data["refresh_token"]
+        resp = await client.get(
+            "/v1/auth/users/tokens/info",
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
+        assert resp.status_code == 200
+        assert resp.json()["username"] == "test"
