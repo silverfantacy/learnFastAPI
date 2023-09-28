980492f9eecb367cad91cf09d777cbee94b71b18
Zero <silverfantacy@gmail.com>
2023-09-28 11:12:22 +0800

    feat: add the database config

--- src/core/config.py
+++ src/core/config.py
@@ -8,6 +8,8 @@ print(PROJECT_PATH)
 
 class Settings(pydantic_settings.BaseSettings):
     authjwt_secret_key: str = "secret"
+    DATABASE_URL: str
     model_config = pydantic_settings.SettingsConfigDict(
         env_file=PROJECT_PATH / ".env",
+        extra="ignore",
     )
